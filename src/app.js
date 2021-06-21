4437611ca9304444b8179faf8ea4447fbe5dcbc9
Aarathy <aarathym96@gmail.com>
2021-06-21 23:31:05 +0530

    Remove Dummy Data

--- src/app.js
+++ src/app.js
@@ -5,34 +5,10 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css'
 import configureStore from './store/configureStore';
 import AppRouter from './routes/AppRouter'
-import {
-    addExpense
-} from './actions/expenses';
-import {
-    setSortByAmount,
-    setTextFilter
-} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses'
 import { Provider } from 'react-redux';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-    description: 'Water Bill',
-    amount: 4500,
-}));
-store.dispatch(addExpense({
-    description: 'Gas Bill',
-    createdAt:1000
-}));
-store.dispatch(addExpense({
-    description: 'Rent',
-    amount:195
-}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
